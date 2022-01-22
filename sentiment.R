2d05b4a44d0291773606bfd2ad63c68d6b7497f9
Keshav Garg <keshavgarg139@gmail.com>
2022-01-22 21:43:01 +0530

    first commit

---
+++ sentiment.R
@@ -0,0 +1,114 @@
+#----
+# loading the libraries
+setwd("M:/MBA/BIM_BusinessAnalytics/Sentiment Analysis")
+
+library(tidyverse)
+library(tidytext)
+library(glue)
+library(stringr)
+
+#----
+# get a list of the files in the input directory
+files <- list.files("M:/MBA/BIM_BusinessAnalytics/Sentiment Analysis/data/")
+
+#----
+# combine the path to the file & 1st file
+fileName <- glue("M:/MBA/BIM_BusinessAnalytics/Sentiment Analysis/data/", files[1], sep = " ")
+#----
+# get rid of any spaces at the end
+fileName <- trimws(fileName)
+
+#----
+# read in the new file
+fileText <- glue(read_file(fileName))
+
+
+#----
+# remove any dollar signs (they're special characters in R)
+fileText <- gsub("\\$", "", fileText) 
+
+#----
+# tokenize
+tokens <- data_frame(text = fileText) %>% unnest_tokens(word, text)
+
+
+#----
+# get the sentiment from the first text: 
+tokens %>%
+  inner_join(get_sentiments("bing")) %>% # pull out only sentiment words
+  count(sentiment) %>% # count the # of positive & negative words
+  spread(sentiment, n, fill = 0) %>% # made data wide rather than narrow
+  mutate(sentiment = positive - negative) # # of positive words - # of negative words
+
+######################################################
+#----
+# write a function that takes the name of a file and returns the # of postive
+# sentiment words, negative sentiment words, the difference & the normalized difference
+GetSentiment <- function(file){
+  # get the file
+  fileName <- glue("M:/MBA/BIM_BusinessAnalytics/Sentiment Analysis/data/", file, sep = " ")
+  # get rid of any sneaky trailing spaces
+  fileName <- trimws(fileName)
+  
+  # read in the new file
+  fileText <- glue(read_file(fileName))
+  # remove any dollar signs (they're special characters in R)
+  fileText <- gsub("\\$", "", fileText) 
+  
+  # tokenize
+  tokens <- data_frame(text = fileText) %>% unnest_tokens(word, text)
+  
+  # get the sentiment from the first text: 
+  sentiment <- tokens %>%
+    inner_join(get_sentiments("bing")) %>% # pull out only sentimen words
+    count(sentiment) %>% # count the # of positive & negative words
+    spread(sentiment, n, fill = 0) %>% # made data wide rather than narrow
+    mutate(sentiment = positive - negative) %>% # # of positive words - # of negative owrds
+    mutate(file = file) %>% # add the name of our file
+    mutate(year = as.numeric(str_match(file, "\\d{4}"))) %>% # add the year
+    mutate(president = str_match(file, "(.*?)_")[2]) # add president
+  
+  # return our sentiment dataframe
+  return(sentiment)
+}
+
+GetSentiment(files[1])
+
+#################################################################
+
+# file to put our output in
+sentiments <- data_frame()
+
+# get the sentiments for each file in our datset
+for(i in files){
+  sentiments <- rbind(sentiments, GetSentiment(i))
+}
+
+# distinguish between Bush Sr. and George W. Bush 
+# correct president in applicable rows
+bushSr <- sentiments %>% 
+  filter(president == "Bush") %>% # get rows where the president is named "Bush"...
+  filter(year < 2000) %>% # ...and the year is before 200
+  mutate(president = "Bush Sr.") # and change "Bush" to "Bush Sr."
+
+# remove incorrect rows
+sentiments <- anti_join(sentiments, sentiments[sentiments$president == "Bush" & sentiments$year < 2000, ])
+
+# add corrected rows to data_frame 
+sentiments <- full_join(sentiments, bushSr)
+
+# summerize the sentiment measures
+summary(sentiments)
+
+######################################################
+
+# plot of sentiment over time & automatically choose a method to model the change
+ggplot(sentiments, aes(x = as.numeric(year), y = sentiment)) + 
+  geom_point(aes(color = president))+ # add points to our plot, color-coded by president
+  geom_smooth(method = "auto") # pick a method & fit a model
+
+######################################################
+
+# plot of sentiment by president
+ggplot(sentiments, aes(x = president, y = sentiment, color = president)) + 
+  geom_boxplot() # draw a boxplot for each president
